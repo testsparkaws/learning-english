cd906390e55c0b257a2eb151160564f3d22ce1dd
raj singh <rajsingh@rajs-MacBook-Air.local>
2023-12-10 10:19:49 +0530

    updated

---
+++ learning-english/learn_english_in_50_days.sql
@@ -0,0 +1,34 @@
+
+### How to Improve Your English 
+
+# Step 1. Before you start speaking well , YOU SHOULD START WRITING 
+  * Grammerly PRO , free 
+  * Grammer Plugin 
+  * Document your thought on tools like Notion 
+  * Start writing 10-15 minute daily 
+
+# Step 2. READ BOOKS ALOUD
+  * Start practise thinking in ENGLISH ,
+  * hindi main soch rahe ho then u are translating english 
+  * Padte hue kabhi stammer mut karo, school main paragraph padte thee. I was so 
+
+# Step 3: BE in FRONT of A MIRROR, and SEE IF YOUR TONGUE CAN SPEAK IN A SMOOTH FLOW 
+  - Writing clear 
+  - Vocabulary Clear 
+  * When You Practise Speech 
+    1. Grammer and Vocuabulary
+    2. Pace and Smoothness 
+    3. Accent and Tonality
+
+################ FOR ABOVE 3 COMPOENENT ****** YOU NEED AT LEAST 50 DAYS *****################
+# Jinka grammer/ Vocabulary kharab hai . Only Writing with a Grammer check plugin + reading Books aloud 
+ * for 15 minutes for 50 DAYS 
+
+# Pace and Smoothness kharab hai : Practise debating / Door to door or stage speaking , 
+ * u can practise in-house too 
+
+# Accent and Tonality : Learn from GOOD Speakers and YT Videos 
+  * there are tons of them 
+
+
+
